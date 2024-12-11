e740032dcced420d25c1f49ae44cbee808ea671d
kunori-kiku <esaif_lyl8888@163.com>
2024-12-11 16:09:33 +0800

    Set expand to false as default

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -444,7 +444,7 @@ export default {
         tfo: false,
         scv: true,
         fdn: false,
-        expand: true,
+        expand: false, // set to false as default to avoid too many rules
         appendType: false,
         insert: false, // 是否插入默认订阅的节点，对应配置项 insert_url
         new_name: true, // 是否使用 Clash 新字段
